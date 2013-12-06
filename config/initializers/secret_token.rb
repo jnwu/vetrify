5c2b14c9b437e72327467379234fb4c579941c0e
Chris Tulip <Christopher.tulip@gmail.com>
2013-12-06 10:41:54 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vetrify::Application.config.secret_key_base = '26fb8dead82ca8417296995ade927587a35484bfa4272a8b7ce22bf142295e822419baba046ace88f720967666acd13d270c7b8a29c6209fd542a041d2d754b9'

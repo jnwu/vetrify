fef4af27f97e454ea07b3ff33a5da564ef4d8b0f
jnwu <jack.wu@live.ca>
2013-12-15 23:17:51 -0800

    Updated session and video tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,22 +5,19 @@ class SessionsControllerTest < ActionController::TestCase
   test "should sign in business user" do
     @user = FactoryGirl.create(:business_user)
     post :create, user_type: "business_user", email: @user.email, password: @user.password
-
+  
     assert_equal @user.id, session[:user_id]
   end
 
-  test "should sign in normal user" do
-    @user = FactoryGirl.create(:profile)
+  test "should sign in applicant" do
+    @user = FactoryGirl.create(:applicant)
     post :create, user_type: "applicant", email: @user.email, password: @user.password
-
+  
     assert_equal @user.id, session[:user_id]
   end
 
   test "should logout user" do
     post :destroy
-
     assert_equal nil, session[:user_id]
   end
-
-
 end

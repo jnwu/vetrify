fbcc3bfbc9e13a17300cffc340c9295273f6bdaa
jnwu <jack.wu@live.ca>
2013-12-15 23:15:01 -0800

    Removed unused models, and migration files
    * Removed migrations: create_profiles, create_companies, create_join_table_profile_post, rename_post_column_company_id_to_business_id, add_business_user_association, add_secure_password_fields
    * Removed unused data models in 'applicants/' and 'businesses/'

--- app/models/applicants/source.rb
+++
@@ -1,3 +0,0 @@
-class Source < ActiveRecord::Base
-  has_and_belongs_to_many :skillgroups
-end
--- app/models/applicants/video.rb
+++
@@ -1,5 +0,0 @@
-class Video < ActiveRecord::Base
-  belongs_to :profile
-
-  has_and_belongs_to_many :questions
-end

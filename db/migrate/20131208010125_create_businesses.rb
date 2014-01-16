0b6d25d985c90091cd2a01e6d62c8d41a811c6fc
jnwu <jack.wu@live.ca>
2014-01-15 22:33:39 -0800

    Updated existing data models
    * Removed null restrictions in business
    * Added image url in applicant

--- db/migrate/20131208010125_create_businesses.rb
+++ db/migrate/20131208010125_create_businesses.rb
@@ -2,21 +2,21 @@ class CreateBusinesses < ActiveRecord::Migration
   def change
     create_table :businesses do |t|
       t.string :name
-      t.string :business_type, null: false
-      t.integer :founded_year, null: false
-      t.string :status, null: false
-      t.string :location, null: false
-      t.integer :size, null: false
+      t.string :business_type
+      t.integer :founded_year
+      t.string :status
+      t.string :location
+      t.integer :size
       t.string :stock_exchange
       t.string :ticker
-      t.text :specialties, null: false
+      t.text :specialties
       t.text :culture
       
-      t.string :domain, null: false
-      t.string :url, null: false
+      t.string :domain
+      t.string :url
       t.string :twitter
       t.string :facebook
-      t.string :linkedin, null: false
+      t.string :linkedin
 
       t.timestamps
     end
--- db/migrate/20131216012549_create_applicants.rb
+++ db/migrate/20131216012549_create_applicants.rb
@@ -4,8 +4,7 @@ class CreateApplicants < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :email
-      t.string :password
-      t.string :password_digest
+      t.string :image
 
       t.timestamps
     end

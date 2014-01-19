3d39cfeaf6325771e2d800d4db406d600f1f0d0e
jnwu <jack.wu@live.ca>
2014-01-19 12:04:46 -0800

    Fixed github helper for retreiving repos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,8 @@ module ApplicationHelper
 			http.read_timeout = 15
 			http.use_ssl = true if (uri.scheme == 'https')
 			http.start do
-				request = Net::HTTP::Get.new(uri.request_uri)
-				return http.request(request)
+				request = Net::HTTP::Get.new uri.request_uri
+				return http.request request
 			end
 		rescue Net::ReadTimeout
 			return nil
@@ -19,11 +19,10 @@ module ApplicationHelper
 
 	class GithubHelper
 		extend ApplicationHelper
-
+		REPOS = "https://api.github.com/user/repos"
 		def self.pull token
-			return nil unless(field or token)
-			uri = URI.parse("https://api.github.com/user/repos?access_token=#{token}")
-			return get uri
+			return nil unless token
+			return JSON.parse(get(URI.parse("#{REPOS}?access_token=#{token}")).body)
 	  	end
 	end
 end

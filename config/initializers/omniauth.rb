62620d8cd5af22ab4e520bfc9fde26da7f7392c9
jnwu <jack.wu@live.ca>
2014-01-20 20:42:42 -0800

    Fixed omniauth initializer bug for missing relation

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	# LinkedIn
-	id = Api.find_by provider: 'linkedin', tag: 'client_id'
-	secret = Api.find_by provider: 'linkedin', tag: 'client_secret' 
-	provider :linkedin, id.key, secret.key, :scope => 'r_fullprofile r_emailaddress', :fields => ["id", "email-address", "first-name", "last-name", "picture-url", "location", "positions", "educations"]
+	if ActiveRecord::Base.connection.table_exists? 'apis'
+		id = Api.find_by provider: 'linkedin', tag: 'client_id'
+		secret = Api.find_by provider: 'linkedin', tag: 'client_secret' 
+		provider :linkedin, id.key, secret.key, :scope => 'r_fullprofile r_emailaddress', :fields => ["id", "email-address", "first-name", "last-name", "picture-url", "location", "positions", "educations"]
 
-	# Github
-	id = Api.find_by provider: 'github', tag: 'client_id'
-	secret = Api.find_by provider: 'github', tag: 'client_secret' 
-	provider :github, id.key, secret.key, scope: "user,repo"
+		# Github
+		id = Api.find_by provider: 'github', tag: 'client_id'
+		secret = Api.find_by provider: 'github', tag: 'client_secret' 
+		provider :github, id.key, secret.key, scope: "user,repo"
+	end
 end

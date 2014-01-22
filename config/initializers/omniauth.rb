3e51a6567732ed641d92189085bf6fd16e783910
Chris Tulip <Christopher.tulip@gmail.com>
2014-01-22 13:09:20 -0800

    bug fix - found some problems with the gihub repo stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :linkedin, '75qairvgqfo2xn', 'V2l4Rn3Efm2SW100', :scope => 'r_fullprofile r_emailaddress', :fields => ["id", "email-address", "first-name", "last-name", "picture-url", "public-profile-url", "location", "positions", "educations"]
-	provider :github, '010d4e6a46d2d8deaf0b', '5d59eb9a1b77eecbe43c77c7cce704e0a960cfd4', scope: 'user,repo'
+	provider :github, '010d4e6a46d2d8deaf0b', '5d59eb9a1b77eecbe43c77c7cce704e0a960cfd4', scope: 'user, public_repo'
 end

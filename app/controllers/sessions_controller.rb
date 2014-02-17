0b242a1f4a1783ec001e005296e7ce9426995793
jnwu <jack.wu@live.ca>
2014-02-16 22:44:21 -0800

    Refactored omniauth handlers
    * Split '/auth/:provider/callback' as separate paths
    * Split 'auth_callback' as separate handlers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,71 +1,67 @@
 require 'date'
 
 class SessionsController < ApplicationController
+  before_action :set_session, only: [:linkedin, :github]
+
   # GET /sessions/new
   def new
-    redirect_to "/auth/linkedin"
+    redirect_to '/auth/linkedin'
   end
 
-  # POST /sessions
-  # POST /sessions.json
-  def create
-    user = Applicant.find_by_email(params[:email])
-
-    if user && user.authenticate(params[:password])
-      session[:user_type] = params[:user_type]
-      session[:user_id] = user.id
-      redirect_to root_path, :notice => "Welcome back, #{user.first_name} #{user.last_name}"
-    else
-      flash.now.alert = "Invalid email or password"
-      render :new
-    end
-  end
-
-  # DELETE /sessions/1
-  # DELETE /sessions/1.json
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
-  end
-
-  def auth_callback
-    auth = request.env['omniauth.auth']
-
-    if auth[:provider] == 'linkedin'
-      a = SessionsHelper::LinkedInHelper.user auth[:info]
-        begin
-          unless a.token
-            a.token = Token.create
-            a.token.key = SecureRandom.base64
-            a.token.key = SecureRandom.base64 while Token.find_by(key: a.token.key) || a.token.key.match(/\+/) || a.token.key.match(/\//)
-            a.token.save
-          end
-
-          # Update entries for user positions and educations
-          SessionsHelper::LinkedInHelper.positions a.id, auth[:extra][:raw_info][:positions][:values]
-          SessionsHelper::LinkedInHelper.educations a.id, auth[:extra][:raw_info][:educations][:values]
-        rescue StandardError => e
-          SessionsHelper::MandrillHelper.raw 'jack.wu@live.ca', "#{e.message}\n\n#{e.backtrace}\n\n#{auth[:extra][:raw_info][:positions]}\n\n#{auth[:extra][:raw_info][:educations]}"
-        end
+  def linkedin        
+    begin
+      a = SessionsHelper::LinkedInHelper.user @auth[:info]
       session[:user_id] = a.id
-    elsif auth[:provider] == 'github'
-      a = Applicant.find_by id: session[:user_id]
-      unless a
-        redirect_to root_url
-        return
-      end
 
-      if a.github_url.nil? or a.github != auth[:info][:urls][:GitHub]
-        a.github_url = auth[:info][:urls][:GitHub]
+      # Update entries for user positions and educations
+      SessionsHelper::LinkedInHelper.positions a.id, @auth[:extra][:raw_info][:positions][:values]
+      SessionsHelper::LinkedInHelper.educations a.id, @auth[:extra][:raw_info][:educations][:values]
+        
+      # Update applicant token
+      unless a.token
+        a.token = Token.create
+        a.token.key = SecureRandom.base64
+        a.token.key = SecureRandom.base64 while Token.find_by(key: a.token.key) || a.token.key.match(/\+/) || a.token.key.match(/\//)
+        a.token.save
+      end
+    rescue StandardError => e
+      SessionsHelper::MandrillHelper.raw 'jack.wu@live.ca', "#{e.message}\n\n#{e.backtrace}\n\n#{@auth[:extra][:raw_info][:positions]}\n\n#{@auth[:extra][:raw_info][:educations]}"
+      session[:user_id] = nil
+    end
+
+    set_redirect
+  end
+
+  def github
+    begin 
+      a = Applicant.find session[:user_id]
+  
+      # Update entries applicant repos
+      SessionsHelper::GithubHelper.repos(@auth[:credentials][:token], a) { |r|
+        SessionsHelper::GithubHelper.languages @auth[:credentials][:token], r
+      }
+
+      # Update applicant github url
+      if a.github_url.nil? or a.github != @auth[:info][:urls][:GitHub]
+        a.github_url = @auth[:info][:urls][:GitHub]
         a.save
       end
-
-      SessionsHelper::GithubHelper.repos(auth[:credentials][:token], a) { |r|
-        SessionsHelper::GithubHelper.languages auth[:credentials][:token], r
-      }
+    rescue StandardError => e
+      SessionsHelper::MandrillHelper.raw 'jack.wu@live.ca', "#{e.message}\n\n#{e.backtrace}"
     end
 
-    flash[:id] = session[:user_id]
-    redirect_to '/applicant', :notice => "Authenticated successfully"
+    set_redirect
+  end
+
+private
+  def set_session
+    @auth = request.env['omniauth.auth']
+    redirect_to root_url unless @auth
+  end
+
+  def set_redirect
+    flash[:id] = session[:user_id] if session[:user_id]
+    redirect_to '/applicant' if flash[:id]
+    redirect_to root_url unless flash[:id]
   end
 end
